ebdfdc5706b4492f885957d55817e0820e4019a9
ishaanbhardwaj10 <ishaanbhardwaj410@gmail.com>
2022-10-26 17:09:41 +0530

    added UC-11 option to switch case in main method

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ namespace EmployeeWage
             Console.WriteLine("Welcome to Employee Wage Computation Program");
             Console.WriteLine("Use Case #9: Compute and Save Total Employee Wage for Multiple Companies");
             Console.WriteLine("Use Case #10: Employee Wage for Multiple Companies using Interface Approach");
+            Console.WriteLine("Use Case #11: Employee Wage of Multiple Companies using API Approach");
+
             Console.Write("Choose a use case number to run the program: ");
             int choice = Convert.ToInt32(Console.ReadLine());
 
@@ -27,6 +29,13 @@ namespace EmployeeWage
                     empWageBuilder.addCompanyEmpWage("Flipkart", 20, 10, 60);
                     empWageBuilder.addCompanyEmpWage("Amazon", 20, 15, 80);
                     empWageBuilder.computeEmpWage();
+                    break;
+                case 11:
+                    EmpWageBuilder empWageBuilderOne = new EmpWageBuilder();
+                    empWageBuilderOne.addCompanyEmpWage("Flipkart", 20, 10, 60);
+                    empWageBuilderOne.computeEmpWage();
+                    empWageBuilderOne.addCompanyEmpWage("Amazon", 20, 15, 80);
+                    empWageBuilderOne.computeEmpWage();
                     break;
                 default:
                     Console.WriteLine("Please enter a valid use case number!");
